825b2735e29e3465eded6880ee5413b2246f1df0
Qifeng Xu <80048100+Chee-Xu@users.noreply.github.com>
2021-03-17 00:32:17 +0000

    Update Haskell.hs

--- Haskell.hs
+++ Haskell.hs
@@ -2,6 +2,14 @@
 Last :: [a] -> a
 Last [x] = x
 Last (x:xs) = Last xs
+
 --Find the 2nd last element of a list. e.g [1..3] return 2
 Last' [x,_]  = x --pattern matching
 Last' (x:xs) = Last'' xs
+
+--find the nth element in a list
+elementAt :: [a] -> Int -> a
+elementAt [] _ = error "out the range"
+elementAt (x:xs) i
+    |i == 1 = x
+    |otherwise =  elementAt xs (i-1)
